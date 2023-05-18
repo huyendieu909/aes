92386ebfd5eda4d3eaee5303ca841979ae4374e6
huyendieu909 <hd911910@gmail.com>
2023-05-18 18:08:12 +0700

    modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include "test.h"
-
+#include "base64.hpp"
+using namespace base64;
 int main(){
     Initialize();
     InputStr();
-    // InputKey();
-    KeyNhapSan();
+    InputKey();
+    // KeyNhapSan();
     KeyExpansion();
 
     cout << "Key: ";for (int x=0;x<16;x++) cout <<hex<< (int)Key[x] << " ";
@@ -13,13 +14,19 @@ int main(){
     for (int i = 0; i<strlen(str);i++) in[i] = str[i];
     cout << "in: "; for (int i =0;i<16;i++) cout << hex <<(int) in[i] << " "; cout << "\n";
     Cipher();
-
-    cout << "out cy: ";for (int i=0 ; i < Nb*4 ; i++) cout <<hex<< (int)out[i] << " ";
+    cout << "out cy: ";for (int i=0 ; i < Nb*4 ; i++) cout << out[i];
     in = out;
+    vector<unsigned char>out64 = out;
+    string s;
+    for (int i=0;i<16;i++) s+= out[i];
+    s = to_base64(s);
+    cout << "\nout cy2: " << s;
+    s = from_base64(s);
+    cout << "\nout cyb64: " << s;
     cout << "\nin cy: ";for (int i=0 ; i < Nb*4 ; i++) cout << hex << (int)in[i] << " ";
     cout << "\n";
     Plain();
-    cout << "out pl: ";for (int i=0 ; i < Nb*4 ; i++) cout << hex << (int)out[i] << " ";
+    cout << "out pl: ";for (int i=0 ; i < Nb*4 ; i++) cout << out[i];
     
     // while (sz < strlen(str)) {
     //     sz = fillBlock();
